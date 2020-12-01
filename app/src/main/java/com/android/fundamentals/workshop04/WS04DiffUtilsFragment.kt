3a53dde8ec5c743700285396d00985a655bcece4
bumis95 <bumis95@gmail.com>
2020-12-01 15:23:06 +0300

    Complete lists(RecyclerView)

--- app/src/main/java/com/android/fundamentals/workshop04/WS04DiffUtilsFragment.kt
+++ app/src/main/java/com/android/fundamentals/workshop04/WS04DiffUtilsFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.fundamentals.R
-import com.android.fundamentals.data.models.Actor
 import com.android.fundamentals.domain.ActorsDataSource
 
 class WS04DiffUtilsFragment : Fragment() {
@@ -16,9 +16,9 @@ class WS04DiffUtilsFragment : Fragment() {
     private lateinit var adapter: WS04ActorsAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_workshop_04, container, false)
     }
@@ -44,10 +44,17 @@ class WS04DiffUtilsFragment : Fragment() {
     }
 
     private fun shuffleActors() {
-        val shuffledList: List<Actor> = ActorsDataSource().getActors().shuffled()
-        adapter.bindActors(shuffledList)
-        // TODO: Replace notifyDataSetChanged for updating the recyclerView to DiffUtil.Callback.
-        adapter.notifyDataSetChanged()
+//        val shuffledList: List<Actor> = ActorsDataSource().getActors().shuffled()
+//        adapter.bindActors(shuffledList)
+//        adapter.notifyDataSetChanged()
+
+        val originalData = ActorsDataSource().getActors()
+        val shuffledData = ActorsDataSource().getActors().shuffled()
+        adapter.bindActors(shuffledData)
+        /* Update adapter */
+        val diffCallback = ActorDiffUtilCallback(originalData, shuffledData)
+        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(diffCallback)
+        diffResult.dispatchUpdatesTo(adapter)
     }
 
     companion object {
